2562a4d0b305e41d18ea7816c4cf9436577752ba
MD. Rakibul Islam Nahid <64752731+RKNahid@users.noreply.github.com>
2023-05-05 22:18:41 +0600

    BeeCrowd Solve

---
+++ Python/1041.py
@@ -0,0 +1,15 @@
+a,b = map(float,input().split())
+if a==0 and b==0:
+    print("Origem")
+elif a==0:
+    print("Eixo Y")
+elif b==0:
+    print("Eixo X")
+elif a>0 and b>0:
+    print("Q1")
+elif a<0 and b>0:
+    print("Q2")
+elif a<0 and b<0:
+    print("Q3")
+else:
+    print("Q4")
