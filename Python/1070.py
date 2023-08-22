818241203691999b4b95a57930fbd3808bd5ae96
MD. Rakibul Islam Nahid <64752731+RKNahid@users.noreply.github.com>
2023-08-22 10:45:48 +0600

    Create 1070.py

---
+++ Python/1070.py
@@ -0,0 +1,15 @@
+a = int(input())
+if a%2==0:
+    print(a+1)
+    print(a+3)
+    print(a+5)
+    print(a+7)
+    print(a+9)
+    print(a+11)
+else:
+    print(a)
+    print(a+2)
+    print(a+4)
+    print(a+6)
+    print(a+8)
+    print(a+10)
