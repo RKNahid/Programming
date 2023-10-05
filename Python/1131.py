17b0ae7732375988fe08ab8879fc98e3b2a65521
MD. Rakibul Islam Nahid <64752731+RKNahid@users.noreply.github.com>
2023-10-05 14:22:57 +0600

    Update 1131.py

--- Python/1131.py
+++ Python/1131.py
@@ -1,2 +1,30 @@
 #runtime error
 
+i,g,e,m=0,0,0,0
+while 1:
+    a,b=list(map(int,input().split()))
+    m+=1
+    if a>b:
+        i+=1
+    elif a<b:
+        g+=1
+    else:
+        e+=1
+    t=0
+    while 1:
+        print("novo granal (1-sim 2-nao)")
+        t = int(input())
+        if t==1:
+            continue
+        if t==2:
+            break
+print(m,"grenais")
+print("Inter:{}".format(i))
+print("Gremio:{}".format(g))
+print("Empates:{}".format(e))
+if i>g:
+    print("Inter venceu mais")
+elif g>i:
+    print("Gremio venceu mais")
+else:
+    print("Nao houve vencedor")
